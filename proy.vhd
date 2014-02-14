bc9fca386808e5ffab29370c05820026d2b6223f
Praxedis Augusto Paloalto Gómez <paurguastx@gmail.com>
2014-02-14 10:02:18 -0600

    Actualización del proyecto
    Ajustes para compilación.

--- proy.vhd
+++ proy.vhd
@@ -40,7 +40,8 @@ port(	A, B: in std_logic_vector(2 downto 0);
 	Cout: out std_logic);
 end component;
 
-signal O, M, N, Z, W, V: std_logic_vector(2 downto 0);
+signal O, M, N, Z: std_logic_vector(2 downto 0);
+signal W, V: std_logic;
 begin
 
 	U1: GAND PORT MAP (A(0), B(0), O(0));
@@ -51,10 +52,10 @@ begin
 	U6: GOR PORT MAP (A(2), B(2), M(2));
 	U7: FA3 PORT MAP (A, B, Cin, N, W);
 	U8: FA3 PORT MAP (A, not B, Cin, Z, V);
-	U9: MUX21 PORT MAP (W, Z, S(1), Cout);
-	U9: MUX41 PORT MAP (N(0), Z(0), O(0), M(0), S, R(0)); 
-	U10: MUX41 PORT MAP (N(1), Z(1), O(1), M(1), S, R(1));
-	U11: MUX41 PORT MAP (N(2), Z(2), O(2), M(2), S, R(2));
+	U9: MUX21 PORT MAP (W, V, S(1), Cout);
+	U10: MUX41 PORT MAP (N(0), Z(0), O(0), M(0), S, R(0)); 
+	U11: MUX41 PORT MAP (N(1), Z(1), O(1), M(1), S, R(1));
+	U12: MUX41 PORT MAP (N(2), Z(2), O(2), M(2), S, R(2));
 
 
 end STRL;
